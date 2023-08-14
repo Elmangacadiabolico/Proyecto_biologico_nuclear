35863332a8318edaf8af7e6a47ea1dd03d166bdb
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-08-14 15:18:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const mongodb = require("mongodb");
-const express = require("express");
-const server = express();
+const initialLipids = 'TAC';
+const varyingLipids = ['ACT', 'ATT', 'ATC'];
+const lipids = ['A', 'T', 'C', 'G'];
+const oppositeLipids = {
+  'A': 'T',
+  'T': 'U',
+  'C': 'G',
+  'G': 'C'
+};
 
+const outputDiv = document.getElementById('output');
+const oppositeOutputDiv = document.getElementById('oppositeOutput');
+const showLipidsButton = document.getElementById('showLipidsButton');
 
+showLipidsButton.addEventListener('click', () => {
+  let lipidSequence = initialLipids;
+
+  for (let i = 0; i < 12; i++) {
+    const randomIndex = Math.floor(Math.random() * lipids.length);
+    lipidSequence += lipids[randomIndex];
+  }
+
+  const randomVaryingIndex = Math.floor(Math.random() * varyingLipids.length);
+  lipidSequence += varyingLipids[randomVaryingIndex];
+
+  outputDiv.textContent = `Secuencia de lípidos: ${lipidSequence}`;
+
+  // Generar la secuencia de lípidos opuestos
+  let oppositeLipidSequence = '';
+  for (let i = 0; i < lipidSequence.length; i++) {
+    const currentLipid = lipidSequence[i];
+    const oppositeLipid = oppositeLipids[currentLipid];
+    oppositeLipidSequence += oppositeLipid;
+  }
+  
+  oppositeOutputDiv.textContent = `Secuencia de lípidos opuestos: ${oppositeLipidSequence}`;
+});
