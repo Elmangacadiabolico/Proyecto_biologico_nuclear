875113f01c1368939c95d730f091fef0687b31be
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-08-15 11:31:27 -0300

    Add files via upload

---
+++ serverd.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 2300;
+
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb+srv://lriosbas:negro@empresasbiologicas.6onjklr.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conexión exitosa a MongoDB');
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/index.html'); // Redirigir a la página de inicio
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}` + '');
+});
