ec0667f4d9ba0ddfb9f962867e27b20c5dee1c1f
estenoesfranco <112403813+estenoesfranco@users.noreply.github.com>
2023-08-15 11:29:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-const initialLipids = 'TAC';
-const varyingLipids = ['ACT', 'ATT', 'ATC'];
-const lipids = ['A', 'T', 'C', 'G'];
+const initialLipids = "TAC";
+const varyingLipids = ["ACT", "ATT", "ATC"];
+const lipids = ["A", "T", "C", "G"];
 const oppositeLipids = {
-  'A': 'T',
-  'T': 'U',
-  'C': 'G',
-  'G': 'C'
+    A: "U",
+    T: "A",
+    C: "G",
+    G: "C",
 };
+const traduccionlol = {};
 
-const outputDiv = document.getElementById('output');
-const oppositeOutputDiv = document.getElementById('oppositeOutput');
-const showLipidsButton = document.getElementById('showLipidsButton');
+const outputDiv = document.getElementById("output");
+const oppositeOutputDiv = document.getElementById("oppositeOutput");
+const showLipidsButton = document.getElementById("showLipidsButton");
+const traducciona = document.getElementById("traduccion");
 
-showLipidsButton.addEventListener('click', () => {
-  let lipidSequence = initialLipids;
+showLipidsButton.addEventListener("click", () => {
+    let lipidSequence = initialLipids;
 
-  for (let i = 0; i < 12; i++) {
-    const randomIndex = Math.floor(Math.random() * lipids.length);
-    lipidSequence += lipids[randomIndex];
-  }
+    for (let i = 0; i < 12; i++) {
+        const randomIndex = Math.floor(Math.random() * lipids.length);
+        lipidSequence += lipids[randomIndex];
+    }
 
-  const randomVaryingIndex = Math.floor(Math.random() * varyingLipids.length);
-  lipidSequence += varyingLipids[randomVaryingIndex];
+    const randomVaryingIndex = Math.floor(Math.random() * varyingLipids.length);
+    lipidSequence += varyingLipids[randomVaryingIndex];
 
-  outputDiv.textContent = `Secuencia de lípidos: ${lipidSequence}`;
+    outputDiv.textContent = `Secuencia de ADN: ${lipidSequence}`;
 
-  // Generar la secuencia de lípidos opuestos
-  let oppositeLipidSequence = '';
-  for (let i = 0; i < lipidSequence.length; i++) {
-    const currentLipid = lipidSequence[i];
-    const oppositeLipid = oppositeLipids[currentLipid];
-    oppositeLipidSequence += oppositeLipid;
-  }
-  
-  oppositeOutputDiv.textContent = `Secuencia de lípidos opuestos: ${oppositeLipidSequence}`;
+    // Generar la secuencia de lípidos opuestos
+    let oppositeLipidSequence = "";
+    for (let i = 0; i < lipidSequence.length; i++) {
+        const currentLipid = lipidSequence[i];
+        const oppositeLipid = oppositeLipids[currentLipid];
+        oppositeLipidSequence += oppositeLipid;
+    }
+
+    oppositeOutputDiv.textContent = `Transcripción: ${oppositeLipidSequence}`;
+
+    let traduccionxd = "";
+    for (let i = 0; i < lipidSequence.length; i++) {
+        const currentLipid = lipidSequence[i];
+        const oppositeLipid = oppositeLipids[currentLipid];
+        traduccionxd += oppositeLipid;
+    }
+
+    traducciona.textContent = `Traducción: ${traduccionxd}`;
 });
